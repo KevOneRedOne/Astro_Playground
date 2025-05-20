aa5d1a26a737c407ec0e1e5763046693fe7b039b
DamienDrozd <damien.drozd@icloud.com>
2025-05-20 10:53:00 +0200

    feat(project): add config for solid, react, svelt, vue, preact'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import solidJs from "@astrojs/solid-js";
+
+import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
+
+import vue from "@astrojs/vue";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), react(), svelte(), vue(), preact()]
+});
